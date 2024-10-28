644c0f659487e0f319585d8c4f1f050aef527821
seokmin <msm0748@gmail.com>
2024-10-28 11:24:39 +0900

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '192.168.0.127', // 또는 0.0.0.0으로 설정해 다른 장치에서도 접근 가능하게 할 수 있음
+    port: 3000,
+  },
+});
